8a09c364380496194e01c8340ee82c26d05a5f2b
unknown <zondit660@gmail.com>
2023-11-29 08:42:42 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function Thankyou(){
+    document.getElementById("d1").style.display = "none"
+    document.getElementById("thankyou").style.display = "block"
+
+}
+
+function btn2(){
+    document.getElementsByClassName("thankyou")[0].style.display = "none"
+}
